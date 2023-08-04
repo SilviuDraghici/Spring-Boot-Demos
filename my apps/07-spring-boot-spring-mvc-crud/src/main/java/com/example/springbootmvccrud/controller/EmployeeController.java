10f05d2a375b5b51cac6a8a4c6235449fcbaea49
Silviu Draghici <silviu.dan.draghici@gmail.com>
2023-08-04 01:07:27 -0400

    07-spring-boot-spring-mvc-crud: Added employee Add

--- my apps/07-spring-boot-spring-mvc-crud/src/main/java/com/example/springbootmvccrud/controller/EmployeeController.java
+++ my apps/07-spring-boot-spring-mvc-crud/src/main/java/com/example/springbootmvccrud/controller/EmployeeController.java
@@ -1,16 +1,15 @@
 package com.example.springbootmvccrud.controller;
 
-import com.example.springbootmvccrud.dao.EmployeeDAO;
 import com.example.springbootmvccrud.entity.Employee;
 import com.example.springbootmvccrud.service.EmployeeService;
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -31,6 +30,22 @@ public class EmployeeController {
 		List<Employee> employees = employeeService.findAll();
 		theModel.addAttribute("employees", employees);
 
-		return "list-employees";
+		return "employees/list-employees";
 	}
+
+	@GetMapping("/add-employee")
+	public String addEmployee(Model model){
+		Employee newEmployee = new Employee();
+		model.addAttribute("employee", newEmployee);
+
+		return "employees/employee-form";
+	}
+
+	@PostMapping("/save")
+	public String addEmployee(@ModelAttribute("employee") Employee employee){
+		employeeService.upsertEmployee(employee);
+
+		return "redirect:/employees/list";
+	}
+
 }
