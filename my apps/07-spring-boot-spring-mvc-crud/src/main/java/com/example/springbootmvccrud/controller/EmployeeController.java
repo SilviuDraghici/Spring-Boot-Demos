d5b9405874b696cced6f4c706ddfe407e2ba4959
Silviu Draghici <silviu.dan.draghici@gmail.com>
2023-08-04 23:09:35 -0400

    07-spring-boot-spring-mvc-crud: Added update and delete.

--- my apps/07-spring-boot-spring-mvc-crud/src/main/java/com/example/springbootmvccrud/controller/EmployeeController.java
+++ my apps/07-spring-boot-spring-mvc-crud/src/main/java/com/example/springbootmvccrud/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.example.springbootmvccrud.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,9 +38,24 @@ public class EmployeeController {
 		return "employees/employee-form";
 	}
 
+	@GetMapping("/update-employee")
+	public String updateEmployee(@RequestParam("employeeId") int id, Model model){
+		Employee employee = employeeService.findById(id);
+		model.addAttribute("employee", employee);
+
+		return "employees/employee-form";
+	}
+
 	@PostMapping("/save")
 	public String addEmployee(@ModelAttribute("employee") Employee employee){
 		employeeService.upsertEmployee(employee);
+
+		return "redirect:/employees/list";
+	}
+
+	@GetMapping("/delete")
+	public String deleteEmployee(@RequestParam("employeeId") int id){
+		employeeService.deleteEmployee(id);
 
 		return "redirect:/employees/list";
 	}
