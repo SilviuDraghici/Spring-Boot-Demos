20f42f5d0bb1c0dabf4d4e0b35c3677a460886a2
Silviu Draghici <silviu.dan.draghici@gmail.com>
2023-08-07 20:21:52 -0400

    08-spring-boot-spring-mvc-security: Users are now read from database.

---
+++ my apps/08-spring-boot-spring-mvc-security/src/main/java/com/example/demosecurity/security/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.example.demosecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsManager dbUsers(DataSource datasource){
+        System.out.println("Creating user details manager!");
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(datasource);
+
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "SELECT user_id, pw, active FROM employee_directory.members WHERE user_id = ?"
+        );
+
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "SELECT user_id, role FROM employee_directory.roles WHERE user_id = ?"
+        );
+
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilter(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+            configurer
+                    .requestMatchers(HttpMethod.GET, "/**").hasRole("EMPLOYEE")
+                    .requestMatchers(HttpMethod.POST, "/**").hasRole("ADMIN")
+                    .requestMatchers(HttpMethod.PUT, "/**").hasRole("ADMIN")
+                    .requestMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
+        );
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(csrf -> csrf.disable());
+
+        http.formLogin(form -> form.permitAll());
+
+        return http.build();
+    }
+
+}
